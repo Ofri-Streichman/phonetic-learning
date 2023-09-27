715869c4540fce9018cece5611cb0ca694cb7bd9
‪Ofri Streichman‬‏ <74958421+Ofri-Streichman@users.noreply.github.com>
2023-09-27 17:04:55 +0100

    setup react-router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import ErrorPage from "./pages/error-page.jsx";
+import Root from "./pages/Root.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    // loader: rootLoader,
+    // children: [
+    //   {
+    //     path: "/Home",
+    //     element: <Home />,
+    //   },
+    //   {
+    //     path: "/Add",
+    //     element: <Add />,
+    //   },
+    //   {
+    //     path: "/WeekList",
+    //     element: <WeekList />,
+    //   },
+    //   {
+    //     path: "/weeks/:weekId/:englishWord",
+    //     element: <Card />,
+    //     loader: cardLoader,
+    //   },
+    // ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/Root.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+// import Header from "../components/Header";
+// import NavBar from "../components/NavBar";
+// import {
+//     Outlet,
+//     useLoaderData,
+//     Link
+// } from "react-router-dom";
+// import { getWeeks } from "./WeekList";
+
+function Root() {
+
+    return (
+        <>
+        <h1>Hello!</h1>
+        </>
+    );
+}
+
+export default Root;
